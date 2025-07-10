87ac857d989ed7ad7a43cf320c66c2536bfe99b3
Saket Kothari <saketkothari722@gmail.com>
2025-07-10 21:54:08 +0530

    feat(trpc): preview prefetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
-'use client';
-import { useTRPC } from '@/trpc/client';
-import { useQuery } from '@tanstack/react-query';
+import { Suspense } from 'react';
+import { Client } from './client';
+import { getQueryClient, trpc } from '@/trpc/server';
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
 
-const Page = () => {
-  const trpc = useTRPC();
-  const { data } = useQuery(trpc.hello.queryOptions({ text: 'bruce wayne' }));
+const Page = async () => {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(
+    trpc.hello.queryOptions({ text: 'Batman using prefetch' })
+  );
 
-  return <div>{JSON.stringify(data)}</div>;
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Client />
+      </Suspense>
+    </HydrationBoundary>
+  );
 };
 
 export default Page;
