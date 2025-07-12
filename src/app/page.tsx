f930ea3a0a2a25c78b091f644650f063bcde9ea2
Saket Kothari <saketkothari722@gmail.com>
2025-07-12 13:39:28 +0530

    ai jobs using inngest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 'use client';
 
 import { toast } from 'sonner';
+import { useState } from 'react';
 import { useTRPC } from '@/trpc/client';
+import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { useMutation } from '@tanstack/react-query';
 
 const Page = () => {
+  const [value, setValue] = useState('');
+
   const trpc = useTRPC();
   const invoke = useMutation(
     trpc.invoke.mutationOptions({
@@ -17,7 +21,11 @@ const Page = () => {
 
   return (
     <div className="p-4 max-w-7xl mx-auto">
-      <Button disabled={invoke.isPending} onClick={() => invoke.mutate({ text: 'John' })}>
+      <Input value={value} onChange={(e) => setValue(e.target.value)} />
+      <Button
+        disabled={invoke.isPending}
+        onClick={() => invoke.mutate({ value: value })}
+      >
         Invoke Background Job
       </Button>
     </div>
